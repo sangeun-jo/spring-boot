a94f79b00a959cfe779fbfe13737c3f05daad69a
sangeun-jo <dog8536@naver.com>
2021-03-08 23:51:34 +0900

    chap10 10.1.2 완료

---
+++ chap10/src/main/java/sej/springexample/SpringApp.java
@@ -0,0 +1,20 @@
+package sej.springexample;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringApp {
+    public static void main(String arg[]) {
+        SpringApplication.run(SpringApp.class);
+    }
+
+    @Bean
+    public CommandLineRunner runner() {
+        return (a) -> {
+            System.out.println("runnable jar running");
+        };
+    }
+}
